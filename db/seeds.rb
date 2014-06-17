1a87b5c853ba7994bf08ebdd20fbb55ddb6c3acf
RoxasShadow <webmaster@giovannicapuano.net>
2014-06-17 23:53:46 +0200

    Use banged-methods in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,26 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create email: 'roxas.shadow@hotmail.it', password: 'lolwut', created_at: 2.years.ago
+user = User.create! email: 'roxas.shadow@hotmail.it', password: 'lolwut', created_at: 2.years.ago
 
-paypal   = user.accounts.create name: 'PayPal',   money: 180.45,  created_at: 2.years.ago
-bancomat = user.accounts.create name: 'Bancomat', money: 2500.00, created_at: 2.years.ago
-wallet   = user.accounts.create name: 'Wallet',   money: 200.00,  created_at: 3.months.ago
+paypal   = user.accounts.create! name: 'PayPal',   money: 180.45,  created_at: 2.years.ago
+bancomat = user.accounts.create! name: 'Bancomat', money: 2500.00, created_at: 2.years.ago
+wallet   = user.accounts.create! name: 'Wallet',   money: 200.00,  created_at: 3.months.ago
 
-paypal.items.create name: 'Pokémon X',      price: 45.00, created_at: 2.months.ago
-paypal.items.create name: 'Child of Light', price: 22.90, details: 'PC', created_at: 2.months.ago
-paypal.items.create name: 'Steins;Gate v1', price: 36.90, details: 'BD', created_at: 1.month.ago
+paypal.items.create! name: 'Pokémon X',      price: 45.00, created_at: 2.months.ago
+paypal.items.create! name: 'Child of Light', price: 22.90, details: 'PC', created_at: 2.months.ago
+paypal.items.create! name: 'Steins;Gate v1', price: 36.90, details: 'BD', created_at: 1.month.ago
 
-bancomat.items.create name: 'Intel Core i7 4670k', price: 220.90, created_at: 10.days.ago
-bancomat.items.create name: 'AMD Sapphire 280x',   price: 170.00, created_at: 10.days.ago
-bancomat.items.create name: 'The world',           price: 699.99, quantity: 99, created_at: 1000.years.ago
+bancomat.items.create! name: 'Intel Core i7 4670k', price: 220.90, created_at: 10.days.ago
+bancomat.items.create! name: 'AMD Sapphire 280x',   price: 170.00, created_at: 10.days.ago
+bancomat.items.create! name: 'The world',           price: 699.99, quantity: 99, created_at: 1000.years.ago
 
 [3.days.ago, 0.days.ago].each do |date|
-  wallet.items.create name: 'Bread',   price: 1.00, quantity: 10, created_at: date
-  wallet.items.create name: 'Water',   price: 1.50, quantity: 5,  created_at: date
-  wallet.items.create name: 'Cookies', price: 2.30, quantity: 3,  created_at: date
+  wallet.items.create! name: 'Bread',   price: 1.00, quantity: 10, created_at: date
+  wallet.items.create! name: 'Water',   price: 1.50, quantity: 5,  created_at: date
+  wallet.items.create! name: 'Cookies', price: 2.30, quantity: 3,  created_at: date
 end
 
-wallet.items.create name: 'Apple',  price: 1.00, quantity: 4,  created_at: 1.day.ago
-wallet.items.create name: 'Kiwi',   price: 1.50, quantity: 5,  created_at: 1.day.ago
-wallet.items.create name: 'Tomato', price: 0.90, quantity: 2,  created_at: 1.day.ago
+wallet.items.create! name: 'Apple',  price: 1.00, quantity: 4,  created_at: 1.day.ago
+wallet.items.create! name: 'Kiwi',   price: 1.50, quantity: 5,  created_at: 1.day.ago
+wallet.items.create! name: 'Tomato', price: 0.90, quantity: 2,  created_at: 1.day.ago
